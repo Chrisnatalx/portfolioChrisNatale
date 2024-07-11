72fe884ec20b1e78a2c166cef9d2204358526331
Chrisnatalx <natale.christian.a@gmail.com>
2024-07-11 20:01:57 -0300

    Test Deployment

--- api/config/db.js
+++ api/config/db.js
@@ -1,26 +1,11 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-import pg from 'pg';
-
-const { Pool } = pg;
-
-const pool = new Pool({
-	connectionString: process.env.POSTGRES_URL,
-});
-
 dotenv.config({ path: '.env' });
-const {
-	DB_USER,
-	DB_PASSWORD,
-	DB_HOST,
-	POSTGRES_USER,
-	POSTGRES_HOST,
-	POSTGRES_PASSWORD,
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const db = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Igportfolio`
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 );
 
 export default db;
