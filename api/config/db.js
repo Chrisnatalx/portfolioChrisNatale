e36aaf346a2f618c99c2545d3938dafef9cb399c
Chrisnatalx <natale.christian.a@gmail.com>
2024-07-12 12:29:17 -0300

    Test dbconection

--- api/config/db.js
+++ api/config/db.js
@@ -5,7 +5,7 @@ dotenv.config({ path: '.env' });
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const db = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+	`postgresql://postgres:OayGDSvwuVhlfuhGyAMXdroEQSHfQVWU@roundhouse.proxy.rlwy.net:22089/railway`
 );
 
 export default db;
